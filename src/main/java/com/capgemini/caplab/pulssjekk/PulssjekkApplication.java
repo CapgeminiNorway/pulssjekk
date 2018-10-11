ad1b013245abe4c0e3fbff28017880706701afbe
mazahir89 <mazahir89@gmail.com>
2018-10-11 11:00:23 +0200

    Jpa CONFIGURATION, Communication with database, writing messages to database

--- src/main/java/com/capgemini/caplab/pulssjekk/PulssjekkApplication.java
+++ src/main/java/com/capgemini/caplab/pulssjekk/PulssjekkApplication.java
@@ -2,7 +2,9 @@ package com.capgemini.caplab.pulssjekk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.capgemini.caplab.pulssjekk.repository")
 @SpringBootApplication
 public class PulssjekkApplication {
 
---
+++ src/main/java/com/capgemini/caplab/pulssjekk/model/Message.java
@@ -0,0 +1,39 @@
+package com.capgemini.caplab.pulssjekk.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="message")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="content")
+    private String content;
+
+    public Message() { }
+
+    public Message(Long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
