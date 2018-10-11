2ccbed9d99d88e92c3f666f4043f4fc6fa0df540
Borgenvik <harald.borgenvik@capgemini.com>
2018-10-11 10:41:19 +0200

    init

---
+++ src/main/java/com/capgemini/caplab/pulssjekk/PulssjekkApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.caplab.pulssjekk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulssjekkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulssjekkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/caplab/pulssjekk/PulssjekkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.capgemini.caplab.pulssjekk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PulssjekkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
