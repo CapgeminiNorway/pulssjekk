4a0df7772588243ede5a4e67f8bd93468ff8d485
mazahir89 <mazahir89@gmail.com>
2018-10-11 14:29:28 +0200

    Jpa CONFIGURATION, Communication with database, writing messages to database

--- src/main/java/com/capgemini/caplab/pulssjekk/PulssjekkApplication.java
+++ src/main/java/com/capgemini/caplab/pulssjekk/PulssjekkApplication.java
@@ -2,14 +2,19 @@ package com.capgemini.caplab.pulssjekk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.capgemini.caplab.pulssjekk.repository")
 @SpringBootApplication
 public class PulssjekkApplication {
+	public static void main(String[] args){
 
-	public static void main(String[] args) {
-		SpringApplication.run(PulssjekkApplication.class, args);
+
+	SpringApplication.run(PulssjekkApplication.class, args);
 	}
+
+
+
 
 }
